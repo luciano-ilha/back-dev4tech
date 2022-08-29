a41e86275bc35b68c2ccc9c64e9ef5cdc27b4407
Leonardo Gomes <leonardo.gomes@tempest.com.br>
2022-08-29 17:21:21 -0300

    fix controller patient

--- src/api/routes/v1/patient.route.js
+++ src/api/routes/v1/patient.route.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const validate = require('express-validation');
+// const validate = require('express-validation');
 const controller = require('../../controllers/patient.controller');
-const { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth');
-const {
-  // listPatients,
-  createPatient,
-  replacePatient,
-  updatePatient,
-} = require('../../validations/patient.validation');
+// const { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth');
+// const {
+//   createPatient,
+//   replacePatient,
+//   updatePatient,
+// } = require('../../validations/patient.validation');
 
 const router = express.Router();
 
@@ -15,20 +14,14 @@ router.param('patientId', controller.load);
 
 router
   .route('/')
-
   .get(controller.list)
+  .post(controller.create);
 
-  .post(authorize(ADMIN), validate(createPatient), controller.create);
-
-router
-  .route('/:patientId')
-
-  .get(authorize(LOGGED_USER), controller.get)
-
-  .put(authorize(LOGGED_USER), validate(replacePatient), controller.replace)
-
-  .patch(authorize(LOGGED_USER), validate(updatePatient), controller.update)
-
-  .delete(authorize(LOGGED_USER), controller.remove);
+// router
+//   .route('/:patientId')
+//   .get(controller.get)
+//   .put(controller.replace)
+//   .patch(controller.update)
+//   .delete(controller.remove);
 
 module.exports = router;
